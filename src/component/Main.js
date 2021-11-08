aa69d0c2c7e57e4d54d79d6d7c5a7eb79debd03a
seondal <sseondal@gmail.com>
2021-11-08 12:57:59 +0900

    [Create] 전체틀 제작

---
+++ src/component/Main.js
@@ -0,0 +1,15 @@
+import Ad from "./Ad";
+import Video from "./Video";
+
+const Main = () => {
+  return (
+    <div>
+      <Ad></Ad>
+      <Video></Video>
+      <Video></Video>
+      <Video></Video>
+    </div>
+  );
+};
+
+export default Main;
