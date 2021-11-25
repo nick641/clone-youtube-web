9fcd097b74bd57bfa4c6cf9b91a9dfdf9a2eb0ef
seondal <sseondal@gmail.com>
2021-11-25 19:19:51 +0900

    #4 complete publish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./component/Header";
 import Sidebar from "./component/Sidebar";
 import Main from "./component/Main";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Sidebar></Sidebar>
-      <Main></Main>
+      <Wrapper>
+        <Sidebar></Sidebar>
+        <Main></Main>
+      </Wrapper>
     </div>
   );
 }
--- src/component/Ad.js
+++ src/component/Ad.js
@@ -1,5 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 276px;
+  height: 241px;
+  font-size: 14px;
+  margin: 8px;
+  text-align: left;
+  gap: 8px;
+`;
+
+const Thumbnail = styled.img`
+  margin-bottom: 10px;
+`;
+
+const Title = styled.div``;
+
+const Detail = styled.div`
+  color: grey;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: grey;
+  gap: 10px;
+`;
+
+const Tag = styled.div`
+  display: flex;
+  background-color: #fbbf2c;
+  border-radius: 3px;
+  width: 30px;
+  height: 18px;
+  font-size: 10px;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+`;
+
+const Channel = styled.div``;
+
 const Ad = () => {
-  return <div>Ad</div>;
+  return (
+    <Wrapper>
+      <Thumbnail src="assets_header/thumbnail.png" />
+
+      <Title>브라우저에서 지금 플레이하세요</Title>
+      <Detail>
+        시간이 아깝지 않은 부분 유료화 게임을 찾으시나요? 이미 찾았어요! 지금
+        확인하세요!
+      </Detail>
+      <Info>
+        <Tag>광고</Tag>
+        <Channel>Hero Wars</Channel>
+      </Info>
+    </Wrapper>
+  );
 };
 
 export default Ad;
